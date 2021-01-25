a480dea2edd545cb23be94a0a5f62ef7beb8c143
epestov <epestov@gmail.com>
2021-01-25 21:24:19 +1100

    added

---
+++ react-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div class="navbar-brand">Grocery List</div>
+                    </nav>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent
